c3195adddcfa768b4c8f4c8f5faa1d1a929e9d46
Ian Ketelings <ian@iank.nl>
2020-06-22 19:08:45 +0200

    Acronis fix

--- acronis/acronis.py
+++ acronis/acronis.py
@@ -3,12 +3,28 @@ import requests
 
 class Acronis:
 
-    def __init__(self, url, tenant_id, tenant_locator):
+    def __init__(self, url, username, password):
         self._url = url
-        self._tenant_id = tenant_id
-        self._tenant_locator = tenant_locator
+        self._username = username
+        self._password = password
 
-    def get_alerts(self, severity, show_deleted):
-        return requests.get(
-            self._url + "/api/alert_manager/v1/alerts?severity=%s&show_deleted=%s" % (severity, show_deleted),
-            headers={'X-Apigw-Tenant-ID': self._tenant_id, 'X-Apigw-Tenant-Locator': self._tenant_locator}).json()
+    def _get_auth_headers(self):
+        payload = dict(grant_type=self._grant_type.name, scope='openid')
+        payload.update(
+            dict(
+                username=self._username,
+                password=self._password,
+                scope='offline_access',
+            )
+        )
+        response = requests.post(
+            f'{self._url}/api/2/idp/token',
+            headers={'Content-Type': 'application/x-www-form-urlencoded'},
+            data=payload,
+        )
+        access_token = response.json().get('access_token')
+        return dict(Authorization=f'Bearer {access_token}')
+
+    def get_alerts(self):
+        return requests.get(self._url + "/api/alert_manager/v1/alerts",
+                            headers=self._get_auth_headers()).json()
